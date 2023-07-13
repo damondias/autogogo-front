212991e20ca2bf237d9035a636d88311090752f2
Levy <levymcruz@gmail.com>
2023-07-13 14:32:09 -0300

    feat: adding sidebar

--- src/pages/Home/HomePage.jsx
+++ src/pages/Home/HomePage.jsx
@@ -1,7 +1,14 @@
-import React from 'react';
+import React, { useState } from 'react';
 import styled from 'styled-components';
+import { FiMenu } from 'react-icons/fi'; // Importe o ícone de menu hambúrguer do React-icons
 
 export default function HomePage() {
+  const [sidebarOpen, setSidebarOpen] = useState(false)
+
+  const toggleSidebar = () => {
+      setSidebarOpen(!sidebarOpen)
+  };
+
   const renderizaAleatorio = () => {
     const cards = [];
     for (let i = 0; i < 30; i++) {
@@ -15,7 +22,13 @@ export default function HomePage() {
   };
 
   return (
-    <Container>
+    <Container sidebarOpen={sidebarOpen}>
+      <Sidebar sidebarOpen={sidebarOpen}>
+        oi
+      </Sidebar>
+      <Button onClick={toggleSidebar}>
+        <FiMenu size={20} />
+      </Button>
       <CardContainer>{renderizaAleatorio()}</CardContainer>
     </Container>
   );
@@ -29,7 +42,10 @@ const Container = styled.div`
   justify-content: center;
   align-items: center;
   border-radius: 50px;
-  padding-bottom: 2rem; padding-top: 2rem;
+  padding-bottom: 2rem;
+  padding-top: 2rem;
+  margin-left: ${props => (props.sidebarOpen ? '200px' : '0')}; // Adiciona uma margem esquerda para acomodar a sidebar
+  transition: margin-left 0.3s ease-in-out;
 `;
 
 const CardContainer = styled.div`
@@ -43,13 +59,35 @@ const CardContainer = styled.div`
 `;
 
 const Card = styled.div`
-    background-color: #cacaca;
-    padding: 1rem;
-    height: 100px;
-    border-radius: 10px;
-    img {
-        width: 80px;
-        border-radius: 5px;
-    }
-
+  background-color: #cacaca;
+  padding: 1rem;
+  height: 100px;
+  border-radius: 10px;
+  img {
+    width: 80px;
+    border-radius: 5px;
+  }
 `;
+
+const Button = styled.button`
+  position: absolute;
+  top: 1rem;
+  left: 1rem;
+  background: none;
+  border: none;
+  cursor: pointer;
+  z-index: 999;
+`;
+
+const Sidebar = styled.div`
+  width: ${props => (props.sidebarOpen ? '200px' : '0')};
+  height: 100vh;
+  background-color: #f0f0f0;
+  position: absolute;
+  top: 0;
+  left: 0;
+  transition: margin-left 0.3s ease-in-out;
+  display: ${props => (props.sidebarOpen ? 'flex' : 'none')};
+  
+`;
+
