4faac47d19a97a840dc537e0218924ab23f9fcb1
Levy <levymcruz@gmail.com>
2023-07-13 13:54:04 -0300

    feat: initialization of HomePage

--- src/pages/Home/HomePage.jsx
+++ src/pages/Home/HomePage.jsx
@@ -1,39 +1,55 @@
 import React from 'react';
 import styled from 'styled-components';
 
-
 export default function HomePage() {
+  const renderizaAleatorio = () => {
+    const cards = [];
+    for (let i = 0; i < 30; i++) {
+      cards.push(
+        <Card key={i}>
+          <img src="https://www.chevrolet.com.br/content/dam/chevrolet/mercosur/brazil/portuguese/index/cars/cars-subcontent/02-images/cruze-sport6-rs-carros.jpg?imwidth=960" alt="" />
+        </Card>
+      );
+    }
+    return cards;
+  };
+
   return (
     <Container>
-      <CardContainer>
-        <Card> testando</Card>
-        <Card>testando</Card>
-        <Card></Card> 
-        <Card></Card>
-      </CardContainer>
+      <CardContainer>{renderizaAleatorio()}</CardContainer>
     </Container>
   );
-};
+}
 
 const Container = styled.div`
-    background-color: yellow;
-    height: 100%;
-    display: flex;
-    justify-content: center;
-    align-items: center;
+  width: 70vw;
+  background-color: yellow;
+  height: 100%;
+  display: flex;
+  justify-content: center;
+  align-items: center;
+  border-radius: 50px;
+  padding-bottom: 2rem; padding-top: 2rem;
 `;
 
 const CardContainer = styled.div`
-    width: 70%;
-    height: 70%;
-    display: flex;
-    flex-wrap: wrap;
-    gap: 0.5rem;
+  width: 70%;
+  height: 70%;
+  display: flex;
+  flex-wrap: wrap;
+  gap: 0.5rem;
+  justify-content: start;
+  align-items: start;
 `;
 
 const Card = styled.div`
-    width: 20px;
-    height: 70px;
-    
-`;
+    background-color: #cacaca;
+    padding: 1rem;
+    height: 100px;
+    border-radius: 10px;
+    img {
+        width: 80px;
+        border-radius: 5px;
+    }
 
+`;
