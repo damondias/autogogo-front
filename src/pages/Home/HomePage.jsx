f9e782dd2d2ca3264cbd51d6d28c3755a49b50d1
Levy <levymcruz@gmail.com>
2023-07-13 21:19:00 -0300

    feat: modalInsere funcionando

--- src/pages/Home/HomePage.jsx
+++ src/pages/Home/HomePage.jsx
@@ -3,24 +3,25 @@ import styled from 'styled-components';
 import { FiMenu } from 'react-icons/fi';
 import axios from 'axios'
 import Swal from 'sweetalert2';
+import { IoInformationCircleOutline } from 'react-icons/io5';
 
 export default function HomePage() {
     const [sidebarOpen, setSidebarOpen] = useState(false);
     const [arrayCarros, setArrayCarros] = useState([]);
   
     useEffect(() => {
-      const fetchData = async () => {
-        try {
-          const response = await axios.get('http://localhost:5000/');
-          setArrayCarros(response.data);
-          console.log(response.data)
-        } catch (error) {
-          console.log(error);
-        }
-      };
-  
-      fetchData();
-    }, []);
+        const getCarros = async () => {
+            await axios.get('http://localhost:5000/').then(res => {
+                setArrayCarros(res.data)
+                // console.log(res.data)
+            }).catch(err => {
+                console.error(err.response.data)
+                alert(err.response.data)
+            })
+        };
+    
+        getCarros();
+    }, [arrayCarros]);
   
 
     const toggleSidebar = () => {
@@ -28,6 +29,7 @@ export default function HomePage() {
     };
 
     const createModalAnnoun = () => {
+        let titulo, marca, km, ano, diaria, localizacao, img, infoExtra;
         Swal.fire({
             title: 'Criar Anúncio',
             html: `
@@ -36,43 +38,73 @@ export default function HomePage() {
                 <input type="text" placeholder="KM Rodados" id="km" class="swal2-input">
                 <input type="text" placeholder="Ano" id="ano" class="swal2-input">
                 <input type="text" placeholder="Faixa de Valor Diária" id="diaria" class="swal2-input">
+                <input type="text" placeholder="Localização" id="localizacao" class="swal2-input">
+                <input type="text" placeholder="URL de uma Foto" id="img" class="swal2-input">
+                <input type="text-area" placeholder="Informações Extras" id="infoExtra" class="swal2-input">
             `,
             showCancelButton: true,
             confirmButtonText: 'Criar',
             cancelButtonText: 'Cancelar',
             focusConfirm: false,
             preConfirm: () => {
-                const titulo = Swal.getPopup().querySelector('#titulo').value;
-                const descricao = Swal.getPopup().querySelector('#descricao').value;
-                const preco = Swal.getPopup().querySelector('#preco').value;
-                const localizacao = Swal.getPopup().querySelector('#localizacao').value;
-                const contato = Swal.getPopup().querySelector('#contato').value;
+                titulo = Swal.getPopup().querySelector('#modelo').value
+                marca = Swal.getPopup().querySelector('#marca').value
+                km = Swal.getPopup().querySelector('#km').value
+                ano = Swal.getPopup().querySelector('#ano').value
+                diaria = Swal.getPopup().querySelector('#diaria').value
+                localizacao = Swal.getPopup().querySelector('#localizacao').value
+                img = Swal.getPopup().querySelector('#img').value
+                infoExtra = Swal.getPopup().querySelector("#infoExtra").value
     
-                // tratativa dos dados inputados
-                console.log(titulo, descricao, preco, localizacao, contato);
+                if (!titulo) return Swal.showValidationMessage("O título deve ser preenchido.")
+                else if (!marca) return Swal.showValidationMessage("A descrição deve ser preenchida.")
+                else if (!km) return Swal.showValidationMessage("Os KMs Rodados devem ser preenchidos.")
+                else if (!ano) return Swal.showValidationMessage("O ano do carro não foi preenchido.")
+                else if (!diaria) return Swal.showValidationMessage("A Faixa de Valor para Diária não foi preenchida.")
+                else if (!localizacao) return Swal.showValidationMessage("A localidade deve ser preenchida.")
+                else if (!img) return Swal.showValidationMessage("O URL da foto deve ser inserido.")
             }
-        });
-      };
+        }).then(res => {
+            if(res.isConfirmed){
+                console.log("Confirmou!")
+                console.log(titulo, marca, km, ano, diaria, localizacao, img, infoExtra)
+                enviarDados(titulo, marca, km, ano, diaria, localizacao, img, infoExtra)
+            }
+        })
+    };
+
+    const enviarDados = (titulo, marca, km, ano, diaria, localizacao, img, infoExtra) => {
+        const data = { titulo, marca, km, ano, diaria, localizacao, img, infoExtra }
+        axios.post('http://localhost:5000/', data).then(response => {
+            console.log('Dados enviados com sucesso:', response.data);
+        }).catch(error => {
+            console.error('Erro ao enviar os dados:', error);
+        })
+    }
 
     return (
         <Container>
             <Sidebar sidebarOpen={sidebarOpen}>
                 <ul>
                     <li><button onClick={createModalAnnoun} id="btnAnnoun">Crie seu Anúncio!</button></li>
-                    <li><button id="btnPoint">Pontos</button></li>
+                    <li><button onClick={() => {console.log(arrayCarros)}} id="btnPoint">Pontos</button></li>
                 </ul>
             </Sidebar>
             <Button onClick={toggleSidebar}>
                 <FiMenu size={20} />
             </Button>
-            <CardContainer>{arrayCarros.map((element, i) => {
+            <CardContainer>{arrayCarros.length > 0 && arrayCarros.map((element, i) => {
               return (
                     <Card key={i}>
-                            <img src="https://www.chevrolet.com.br/content/dam/chevrolet/mercosur/brazil/portuguese/index/cars/cars-subcontent/02-images/cruze-sport6-rs-carros.jpg?imwidth=960" alt="" />
-                            <h2>Modelo: {element.data.nome} {element.data.modelo}</h2>
-                            <h3>Marca: {element.data.marca}</h3>
-                            <h3>KM: {element.data.km.toString().replace(/\B(?=(\d{3})+(?!\d))/g, '.')}km</h3>
-                            <h2>Diária: R${element.data.valor.toFixed(2).replace('.', ',').toString().replace(/\B(?=(\d{3})+(?!\d))/g, '.')}</h2>
+                            <img src={element.img} alt="" /> 
+                            <h2>Modelo: {element.titulo}</h2>
+                            <h3>Marca: {element.marca}</h3>
+                            <h3>KM: {element.km.toString().replace(/\B(?=(\d{3})+(?!\d))/g, '.')}km</h3>
+                            <h2>Diária: R${element.valor}</h2>
+                            <FooterCard>
+                                <p>Alugar</p>
+                                <IoInformationCircleOutline cursor={'pointer'} size={24} />
+                            </FooterCard>
                     </Card>
                     )
             })}</CardContainer>
@@ -90,7 +122,7 @@ const Container = styled.div`
     border-radius: 50px;
     padding-bottom: 2rem;
     padding-top: 2rem;
-    /* margin-top: 20rem; */
+    margin-top: 6rem;
 `;
 
 const CardContainer = styled.div`
@@ -107,10 +139,14 @@ const CardContainer = styled.div`
 const Card = styled.div`
     background-color: #ffdfaf;
     padding: 1rem;
-    /* height: 100px; */
+    display: flex;
+    flex-direction: column;
+    gap: 0.25rem;
+    height: 14em;
     border-radius: 10px;
     img {
-        width: 80px;
+        margin:auto;
+        width: 150px;
         border-radius: 5px;
   }
 `;
@@ -169,3 +205,10 @@ const Sidebar = styled.div`
         }
     }
 `;
+
+const FooterCard = styled.div`
+    margin-top: 0.5rem;
+    display: flex;
+    justify-content: space-between;
+    align-items: center;
+`
