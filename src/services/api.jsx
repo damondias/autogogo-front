afdfa00175494321a6a69880fa58ff8dfa66998e
Damon <damondias@gmail.com>
2023-07-17 14:01:44 -0400

    feauture: post checkout

--- src/services/api.jsx
+++ src/services/api.jsx
@@ -12,16 +12,24 @@ function createConfig(token) {
 
 async function createUser(user) {
     await axios.post(`${BASE_URL}/sign-up`, user);
-  }
+}
   
-  async function login(data) {
-    const token = await axios.post(`${BASE_URL}/sign-in`, data);
-    return token;
-  }
+async function login(data) {
+  const token = await axios.post(`${BASE_URL}/sign-in`, data);
+  return token;
+}
+
+async function createReserve(token, carros, total) {
+ 
+  const config = createConfig(token);
+  await axios.post(`${BASE_URL}/reserve`, {carros, total}, config);
+
+}
 
 const api ={
     createUser,
     login,
+    createReserve,
 
 }
 
